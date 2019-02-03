97b87e4529c51ec14082a3be1f7d541e6f72d6bb
tangs <tangsong1988kaka@gmail.com>
2019-02-03 22:05:02 +0800

    Update.

--- src/1.ts
+++ src/1.ts
@@ -1,5 +1,17 @@
-for(var i = 0; i < 10; ++i) {
-    for(var i = 0; i < 10; ++i) {
+console.log("var:");
+for(var i = 0; i < 4; ++i) {
+    for(var i = 0; i < 4; ++i) {
         console.log(i);
     }
 }
+
+console.log("let:");
+for(let i = 0; i < 4; ++i) {
+    for(let i = 0; i < 4; ++i) {
+        console.log(i);
+    }
+}
+
+const num = 1;
+const str = `num:${num}`;
+console.log(str);
