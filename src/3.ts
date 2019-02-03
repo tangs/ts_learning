f89bc2b05cd59dd879e1206a6597e9e5c5e44ce9
tangs <tangsong1988kaka@gmail.com>
2019-02-03 23:16:58 +0800

    Update.

--- src/3.ts
+++ src/3.ts
@@ -9,9 +9,14 @@ func3_1(o);
 
 interface inter3_1 {
     txt: string;
+    a?: number
 };
 
 const func3_2 = (obj: inter3_1) => {
     console.log(obj.txt);
+    if (obj.a) {
+        console.log(obj.a);
+    }
 };
 func3_2({txt: 'ddd'});
+func3_2({txt: 'ddd', a: 33});
