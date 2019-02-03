7043a944db9c5c4667d7cb429b19416700d4b0f8
tangs <tangsong1988kaka@gmail.com>
2019-02-03 22:15:47 +0800

    Update.

--- src/1.ts
+++ src/1.ts
@@ -13,5 +13,23 @@ for(let i = 0; i < 4; ++i) {
 }
 
 const num = 1;
-const str = `num:${num}`;
+const str = `num:${num}
+
+end.`;
 console.log(str);
+
+let func1 = () => {
+    return 10;
+};
+const num1 = func1();
+let x: [number, string, number];
+x = [1, '33', 3];
+console.log(num1);
+
+enum Color {Red, Green, Blue};
+let c: string = Color[2];
+let c1: Color = Color.Blue;
+console.log(c);
+console.log(Color.Blue);
+console.log(c1.toString());
+console.log(c1 === 2);
