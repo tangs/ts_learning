cb089eda792a8b0aadbef131b4bbc0e2f3c322dc
tangs <tangsong1988kaka@gmail.com>
2019-02-14 23:45:04 +0800

    learing getter setter.

--- src/4.ts
+++ src/4.ts
@@ -22,3 +22,21 @@ a.fun();
 (a as B).fun();
 b.fun();
 (b as C).fun();
+
+class D {
+    public _name: string;
+
+    get name(): string {
+        console.log(`getName:${this._name}.`);
+        return this._name;
+    }
+
+    set name(name: string) {
+        console.log(`setName(${name}).`)
+        this._name = name;
+    }
+}
+
+const d = new D();
+d.name = 'tangs';
+const name1 = d.name;
