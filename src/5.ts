86f0ff2f298f3c6915cf113d95ee3008f5b0031c
tangs <tangsong1988kaka@gmail.com>
2019-03-09 23:36:17 +0800

    Study function overloads.

--- src/5.ts
+++ src/5.ts
@@ -1,8 +1,24 @@
 {
-    let arr: any[] = [];
-    // arr.push(1, 3, 5);
-    arr.push(1, 3, 4);
-    for (const [k, v] of arr.entries()) {
-        console.log(k, v);
+    // let arr: any[] = [];
+    // // arr.push(1, 3, 5);
+    // arr.push(1, 3, 4);
+    // for (const [k, v] of arr.entries()) {
+    //     console.log(k, v);
+    // }
+}
+
+{
+    function fun4_1(x: number): string;
+    function fun4_1(x: string): string;
+    function fun4_1(x: any) {
+        const type = typeof x;
+        if (type === 'number') {
+            return 'number';
+        } else if (type === 'string') {
+            return 'string';
+        }
     }
+    console.log(fun4_1(100));
+    console.log(fun4_1('100'));
+    // fun4_1([1]);
 }
