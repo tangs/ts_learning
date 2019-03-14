728dccfed8677340d17793bfa6b83dd322f68074
tangs <tangsong1988kaka@gmail.com>
2019-03-14 16:03:34 +0800

    update.

---
+++ src/8.ts
@@ -0,0 +1,30 @@
+{
+    const x = [0, '1', null];
+    console.log(x);
+
+    class D {}
+    class A extends D {}
+    class B extends D {}
+    class C extends D {}
+    const y: D[] = [new A(), new B(), new C()];
+    console.log(y);
+    let z: A | B = null;
+    z = new A();
+    console.log(z);
+}
+
+{
+    // interface Inter {
+    //     (arg1: Number): string;
+    // }
+    class A {
+        func(callback: {(arg1: Number): boolean}): void {
+            console.log(callback(111));
+        }
+    }
+    const a = new A();
+    a.func((a): boolean => {
+        console.log(a);
+        return true;
+    });
+}
