c36426fe0e540058e66d99b4ee7b137af29b512d
tangs <tangsong1988kaka@gmail.com>
2019-03-14 16:51:43 +0800

    update.

--- src/9.ts
+++ src/9.ts
@@ -12,3 +12,28 @@
     const a: A & B = { x: 1, y: 3, func: () => true };
     console.log(a);
 }
+
+{
+    class A {
+        x: number;
+        y: number;
+        constructor(x: number, y: number) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class B {
+        x: number;
+        z: number;
+        constructor(x: number, z: number) {
+            this.x = x;
+            this.z = z;
+        }
+    }
+    const func = (): A | B => {
+        return new B(10, 20);
+    };
+    let a: A | B = new A(1, 2);
+    a = func();
+    console.log(a.x);
+}
