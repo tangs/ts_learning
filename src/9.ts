14b31db9e54569efbe7860de8fdfbe674ecef3ca
tangs <tangsong1988kaka@gmail.com>
2019-03-15 11:15:00 +0800

    update

--- src/9.ts
+++ src/9.ts
@@ -50,3 +50,44 @@
         console.log(a.y);
     }
 }
+
+{
+    type type1 = (a: number, b: string) => string;
+    const func: type1 = (a, b) => {
+        return a.toString() + b.toString();
+    };
+    console.log(func(123, '55555'));
+}
+
+{
+    type type1 = number | string;
+    function func(a: 'a'): string;
+    function func(b: 22): number;
+    function func(a: type1): type1 {
+        return a;
+    }
+    console.log(typeof func('a'));
+}
+
+{
+    type type1 = 1 | 2 | 3;
+    type type2 = '1' | '2';
+    let a: type1 = 1;
+    let b: type2 = '1';
+    // a = 43;
+    console.log(a, b);
+}
+
+{
+    interface Inter1 {
+        type: 'type1';
+        x: number;
+    }
+    interface Inter2 {
+        type: 'type2';
+    }
+    const func = (a: Inter1 | Inter2): string => {
+        return a.type;
+    };
+    console.log(func({ type: 'type2' }));
+}
